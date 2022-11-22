00dfbb2e92d32356dcd283740880303b2678b271
Amani Maklad <amani.maklad.bkr@gmail.com>
2022-11-22 11:12:05 +0100

    1

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_21_154042) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_21_154545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.text "comment"
+    t.date "date_from"
+    t.date "date_to"
+    t.bigint "user_id", null: false
+    t.bigint "castle_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["castle_id"], name: "index_bookings_on_castle_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "castles", force: :cascade do |t|
     t.string "name"
@@ -39,5 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_154042) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "castles"
+  add_foreign_key "bookings", "users"
   add_foreign_key "castles", "users"
 end
