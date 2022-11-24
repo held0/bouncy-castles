ebb90a825f7e4a88f93c5786c3570dfb11d15191
Amani <amani.maklad.bkr@gmail.com>
2022-11-24 15:47:20 +0100

    adding avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
   # validates :email, uniqueness: true
 end
